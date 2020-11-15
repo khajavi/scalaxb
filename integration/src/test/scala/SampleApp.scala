1209512f140e5bc173d641a1393008986a8b040b
Milad <khajavi@gmail.com>
2020-11-15 19:51:08 +0330

    add sample app that generate scalaxb files in tmp directory.

---
+++ integration/src/test/scala/SampleApp.scala
@@ -0,0 +1,11 @@
+import java.io.File
+
+import scalaxb.compiler.Module
+import scalaxb.compiler.xsd.Driver
+
+object SampleApp extends App {
+  val module: Module = new Driver
+  val tmpDir = new File("tmp")
+  val input = new File("integration/src/test/resources/datetime.xsd")
+  module.process(input, "foo", tmpDir)
+}
